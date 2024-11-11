7c9461a354396bb86e72719e46ad6de917d4bc1f
Henrik Gustafsson <karlhenrik.gustafsson@gmail.com>
2024-11-11 15:11:17 +0100

    Small Updates
    Fixed some small things.

--- CheckImageViability.m
+++ CheckImageViability.m
@@ -2,7 +2,7 @@ function [outputImage] = CheckImageViability(inputImg, req)
 
 inputImageSize = size(inputImg);
 
-% Check if the smalles dimension can fith the required amount of images:
+% Check if the smalles dimension can fit the required amount of images:
 if(min(inputImageSize(2),inputImageSize(1)) < req)
     ScaleNeeded = req / min(inputImageSize(2),inputImageSize(1));
     outputImage = imresize(inputImg, ScaleNeeded);
@@ -11,7 +11,7 @@ if(min(inputImageSize(2),inputImageSize(1)) < req)
     disp("it was scaled to " + outSize(2) + "x" +outSize(1) + "px to fit the requirement!");
 else
     outputImage = inputImg;
-    disp("The input image is not too small, proceding!");
+    disp("The input image is not too small, proceeding!");
 end
 
 end
